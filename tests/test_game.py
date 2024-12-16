deb42be9dd23c993cef20ec6ce5288caef269ed9
donatien konan <donatien.konan.pro@gmail.com>
2024-12-16 23:34:19 +0000

    add a new testgame refine code

--- tests/test_game.py
+++ tests/test_game.py
@@ -53,6 +53,7 @@ class TestGame:
 # tests/test_game.py
 # [...]
 #
+#
     def test_unknown_word_is_invalid(self):
         """A word that is not in the English dictionary should not be valid"""
         new_game = Game()
